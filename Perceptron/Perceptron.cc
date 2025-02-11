dd9b456c14a53127b60b50388c84f8d221ab9fcb
Ginescag <ginescag_2@outlook.es>
2025-02-12 00:45:49 +0100

    added a visual main to understand perceptrons

--- Perceptron/Perceptron.cc
+++ Perceptron/Perceptron.cc
@@ -1,3 +1,4 @@
+#include <GL/glut.h>
 #include <iostream>
 #include <vector>
 #include <cmath>
@@ -10,19 +11,11 @@ using namespace std;
 //If the sum is greater than a certain threshold, the perceptron outputs 1, otherwise it outputs 0. 
 //The perceptron can be trained to recognize patterns in data by adjusting the weights. The perceptron is the building block of more complex neural networks.
 
-// int sum(vector<int> inputs, vector<int> weights) {
-//     int sum = 0;
-//     for (int i = 0; i < inputs.size(); i++) {
-//         sum += inputs[i] * weights[i];
-//     }
-//     return sum;
-// }
-
-
 class Perceptron {
     private:
         double bias;
         vector<double> weights;
+        double learning_rate = 0.1;
     public:
         Perceptron(){
             this->bias = 0;
@@ -78,13 +71,113 @@ class Perceptron {
             }
             cout << endl;
         }
+
+        void train(const vector<double>& inputs, const int& target){
+            int guess = this->guess(inputs);
+            double error = target - guess;
+
+            //tune all the weights
+            for(int i = 0; i < this->weights.size(); i++){
+                this->weights[i] += error * inputs[i] * this->learning_rate;
+            }
+        }
 };
 
-int main() {
+// Global variables for visualization
+vector<vector<double>> points;
+vector<int> labels;
+vector<bool> correct_guesses;
+    Perceptron perceptron;
+int current_iteration = 0;
+const int TRAINING_POINTS = 100;
+const int ITERATIONS = 100;
+
+void display() {
+    glClear(GL_COLOR_BUFFER_BIT);
+    
+    // Draw coordinate axes
+    glColor3f(0.5, 0.5, 0.5);
+    glBegin(GL_LINES);
+        glVertex2f(-1.0, 0.0);
+        glVertex2f(1.0, 0.0);
+        glVertex2f(0.0, -1.0);
+        glVertex2f(0.0, 1.0);
+    glEnd();
+
+    // Draw target line (y = 2x)
+    glColor3f(0.0, 0.0, 0.0);
+    glBegin(GL_LINES);
+        glVertex2f(-1.0, -2.0);
+        glVertex2f(1.0, 2.0);
+    glEnd();
+
+    // Draw points
+    glPointSize(5.0);
+    for(size_t i = 0; i < points.size(); i++) {
+        if(current_iteration > 0) {
+            if(correct_guesses[i]) {
+                glColor3f(0.0, 1.0, 0.0);  // Green for correct
+            } else {
+                glColor3f(1.0, 0.0, 0.0);  // Red for incorrect
+            }
+        } else {
+            glColor3f(0.0, 0.0, 0.0);  // Black for initial state
+        }
+
+        glBegin(GL_POINTS);
+            glVertex2f(points[i][0]/3.0, points[i][1]/3.0);  // Scale down to fit in [-1,1]
+        glEnd();
+    }
+
+    glutSwapBuffers();
+}
+
+void keyboard(unsigned char key, int x, int y) {
+    if(key == ' ' && current_iteration < ITERATIONS) {
+        // Train one iteration
+        int correct_predictions = 0;
+        for(int j = 0; j < TRAINING_POINTS; j++) {
+            int guess = perceptron.guess(points[j]);
+            correct_guesses[j] = (guess == labels[j]);
+            if(correct_guesses[j]) correct_predictions++;
+            perceptron.train(points[j], labels[j]);
+        }
+
+        double accuracy = (correct_predictions * 100.0) / TRAINING_POINTS;
+        cout << "Iteration " << current_iteration + 1 << ": " << accuracy << "%" << endl;
+        perceptron.printP();
+        current_iteration++;
+        
+        glutPostRedisplay();
+    }
+}
+
+int main(int argc, char** argv) {
     srand(static_cast<unsigned>(time(0)));
-    Perceptron p;
-    p.printP();
-    vector<double> inputs = {0.5, 3};
-    cout << p.guess(inputs) << endl;
+    
+    // Generate training data
+    correct_guesses.resize(TRAINING_POINTS);
+    for(int i = 0; i < TRAINING_POINTS; i++) {
+        double x = (rand() / double(RAND_MAX)) * 6 - 3;
+        double y = (rand() / double(RAND_MAX)) * 6 - 3;
+        points.push_back({x, y});
+        labels.push_back(y > 2 * x ? 1 : -1);
+    }
+
+    // Initialize GLUT
+    glutInit(&argc, argv);
+    glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB);
+    glutInitWindowSize(800, 800);
+    glutCreateWindow("Perceptron Learning");
+
+    // Set white background
+    glClearColor(1.0, 1.0, 1.0, 1.0);
+
+    // Register callbacks
+    glutDisplayFunc(display);
+    glutKeyboardFunc(keyboard);
+
+    // Start main loop
+    glutMainLoop();
     return 0;
 }
